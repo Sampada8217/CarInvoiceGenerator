ddbdfecd6f83b0ceb2f0e57a2556c2885360ea13
sampada8217 <sampadatirthkar@gmail.com>
2020-05-30 13:37:16 +0530

    Added Test Case for Calculating Minimum Fare

--- src/test/java/com/InvoiceGenerator/InvoiceServiceTest.java
+++ src/test/java/com/InvoiceGenerator/InvoiceServiceTest.java
@@ -13,4 +13,12 @@ public class InvoiceServiceTest {
         Assert.assertEquals(25,fare,0.0);
     }
 
+    @Test
+    public void givenLessDistanceAndTime_shouldReturnMinFare() {
+        InvoiceService invoiceService = new InvoiceService();
+        double distance=0.1;
+        int time=1;
+        double fare= invoiceService.calculateFare(distance,time);
+        Assert.assertEquals(5,fare,0.0);
+    }
 }
